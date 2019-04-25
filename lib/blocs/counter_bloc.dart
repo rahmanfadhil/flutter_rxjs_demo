34d4c3b6b99b396bf08626126383a7a373eb0e82
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-25 14:38:27 +0700

    Create counter service with get_it and rxdart

--- lib/blocs/counter_bloc.dart
+++ lib/blocs/counter_bloc.dart
@@ -1,28 +1,43 @@
-import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
-class CounterBloc extends InheritedWidget {
-  final Map<String, int> _counter = {'val': 0};
+// class CounterBloc extends InheritedWidget {
+//   final Map<String, int> _counter = {'val': 0};
 
-  int get counter {
-    return _counter['val'];
+//   int get counter {
+//     return _counter['val'];
+//   }
+
+//   CounterBloc({Widget child}) : super(child: child);
+
+//   void increment() {
+//     _counter['val']++;
+//   }
+
+//   void decrement() {
+//     _counter['val']--;
+//   }
+
+//   @override
+//   bool updateShouldNotify(InheritedWidget oldWidget) {
+//     return true;
+//   }
+
+//   static CounterBloc of(BuildContext context) {
+//     return context.inheritFromWidgetOfExactType(CounterBloc);
+//   }
+// }
+
+class Counter {
+  BehaviorSubject _counter = BehaviorSubject.seeded(0);
+
+  Observable get stream$ => _counter.stream;
+  int get counter => _counter.value;
+
+  increment() {
+    _counter.add(counter + 1);
   }
 
-  CounterBloc({Widget child}) : super(child: child);
-
-  void increment() {
-    _counter['val']++;
-  }
-
-  void decrement() {
-    _counter['val']--;
-  }
-
-  @override
-  bool updateShouldNotify(InheritedWidget oldWidget) {
-    return true;
-  }
-
-  static CounterBloc of(BuildContext context) {
-    return context.inheritFromWidgetOfExactType(CounterBloc);
+  decrement() {
+    _counter.add(counter - 1);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
 import './blocs/counter_bloc.dart';
 
-import './counter_manager.dart';
+GetIt getIt = GetIt();
 
-void main() => runApp(MyApp());
+void main() {
+  getIt.registerSingleton<Counter>(Counter());
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,8 +18,34 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Inherited Widget'),
         ),
-        body: CounterBloc(child: CounterManager()),
+        body: CounterManager(),
       ),
+    );
+  }
+}
+
+class CounterManager extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final counterService = getIt.get<Counter>();
+
+    return Column(
+      children: <Widget>[
+        StreamBuilder(
+          stream: counterService.stream$,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return Text(snapshot.data.toString());
+          },
+        ),
+        RaisedButton(
+          child: Text('Increase'),
+          onPressed: () => counterService.increment(),
+        ),
+        RaisedButton(
+          child: Text('Decrease'),
+          onPressed: () => counterService.decrement(),
+        )
+      ],
     );
   }
 }
