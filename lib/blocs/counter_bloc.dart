ac7f586dbeb3eadf98a22dc78ced8f870a6b736a
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-25 14:26:09 +0700

    Change state from bloc

--- lib/blocs/counter_bloc.dart
+++ lib/blocs/counter_bloc.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CounterBloc extends InheritedWidget {
-  final String hello = 'world';
+  final Map<String, int> _counter = {'val': 0};
+
+  int get counter {
+    return _counter['val'];
+  }
 
   CounterBloc({Widget child}) : super(child: child);
+
+  void increment() {
+    _counter['val']++;
+  }
+
+  void decrement() {
+    _counter['val']--;
+  }
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
--- lib/counter_manager.dart
+++ lib/counter_manager.dart
@@ -5,12 +5,24 @@ import './blocs/counter_bloc.dart';
 class CounterManager extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final String hello = CounterBloc.of(context).hello;
+    final CounterBloc bloc = CounterBloc.of(context);
 
-    print(hello);
-
-    return Center(
-      child: Text('Counter manager'),
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return Column(
+          children: <Widget>[
+            Text(bloc.counter.toString()),
+            RaisedButton(
+              child: Text('Increase'),
+              onPressed: () => setState(() => bloc.increment()),
+            ),
+            RaisedButton(
+              child: Text('Decrease'),
+              onPressed: () => setState(() => bloc.decrement()),
+            )
+          ],
+        );
+      },
     );
   }
 }
